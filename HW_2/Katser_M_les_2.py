5c6fdbbd31189b6e8120f3aadb6c2023c3b9352d
ankiav <anklavvanlove@gmail.com>
2022-10-03 22:54:37 +0300

    uploaded homework 2 lesson

--- HW_2/Katser_M_les_2.py
+++ HW_2/Katser_M_les_2.py
@@ -7,7 +7,8 @@ var_N_M, where N - task number, M - variable number
 
 print('HOMEWORK LESSON 2, TASK 1: creating 3 variables with same data and id')
 var_1_1 = 2
-var_1_2 = var_1_3 = var_1_1  # Initialisation of var_1_2 and var_1_3 which equals var_1_1
+# Initialisation of var_1_2 and var_1_3 which equals var_1_1
+var_1_2 = var_1_3 = var_1_1
 
 print(id(var_1_1), id(var_1_2), id(var_1_3), sep='    ')
 
@@ -16,9 +17,15 @@ print('True') if id(var_1_1) == id(var_1_2) and \
                  id(var_1_2) == id(var_1_3) else print('False')
 # endregion
 
-"""HOMEWORK LESSON 2, TASK 2: creating 2 variables with the same data and different id"""
+"""
+HOMEWORK LESSON 2, TASK 2:
+creating 2 variables with the same data and different id
+"""
 
-print('HOMEWORK LESSON 2, TASK 2: creating 2 variables with the same data and different id')
+print(
+      'HOMEWORK LESSON 2, TASK 2: '
+      'creating 2 variables with the same data and different id'
+)
 
 var_2_1 = [1, 2]
 var_2_2 = [1, 2]
@@ -34,6 +41,7 @@ HOMEWORK LESSON 2, TASK 3_1: changing variables in task 1
 
 var_1_2 = str(var_1_1)
 var_1_3 = tuple(str(var_1_1))
+
 print(id(var_1_1), id(var_1_2), id(var_1_3[0]))
 
 print('True') if id(var_1_1) == id(var_1_2) and \
@@ -53,9 +61,12 @@ print(id(var_2_1), id(var_2_2))
 
 main_string = input("Input something: ")
 """
-[::2] means get chars id=0,2... and so on with step = 2 - this is logic to get ODD chars
-[1::2] means get chars id=1,3... and so on with step = 2 - this is logic to get EVEN chars
+[::2] means get chars id=0,2... and so on with 
+step = 2 - this is logic to get ODD chars
+[1::2] means get chars id=1,3... and so on with 
+step = 2 - this is logic to get EVEN chars
 """
+
 odd_string = main_string[::2]
 even_string = main_string[1::2]
 
