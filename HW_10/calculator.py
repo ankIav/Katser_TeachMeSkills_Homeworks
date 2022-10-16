6ffd461feaaf4b364ef425184addba437940ba0f
ankiav <anklavvanlove@gmail.com>
2022-10-16 17:20:03 +0300

    almost done ver linear calc with some errors about correct inputing

--- HW_10/calculator.py
+++ HW_10/calculator.py
@@ -6,6 +6,6 @@ from classes import Calculator
 field = input('Enter the expression:\n').replace(' ', '').replace(',', '.')
 calculator = Calculator(field)
 
-print(str(calculator))
-print(calculator._plus())
+# print(repr(calculator))
+print(calculator.calculate())
 
--- HW_10/classes.py
+++ HW_10/classes.py
@@ -9,7 +9,6 @@ operators = {
     'minus': '-',
     'multiply': '*',
     'division': '/',
-    'equal': '=',
 }
 
 
@@ -47,6 +46,19 @@ class Calculator:
             ):
                 string[i + 1] = f'-{string[i + 1]}'
                 del string[i]  # after transport minus to num delete this el.
+
+            # numbers like -.123 and .123 will addict 0 for readable
+            while (
+                '-.' in string[i]
+                and string[i].find('-.') == 0
+            ):
+                string[i] = string[i].replace("-.", "-0.")
+
+            while (
+                '.' in string[i]
+                and string[i].find('.') == 0
+            ):
+                string[i] = string[i].replace(".", "0.")
 
         return string  # string now is list ['num', '+', '-num'...] format
 
@@ -98,13 +110,58 @@ class Calculator:
 
         return ' '.join(map(str, self.expression))
 
-    def _plus(self):
+    # def _plus(self):
+    #     """
+    #     :return:
+    #     """
+    #     expression = self.str_to_int_float(self.expression)
+    #     for num in expression:
+    #         while num not in operators.values():
+    #             self.result += num
+    #             break
+    #     return f'{str(self)} = {self.result}'
+
+    @staticmethod
+    def __uh(expression, i):
+        del expression[i], expression[i]
+        expression.insert(0, 'del')
+        expression.insert(0, 'del')
+
+    def calculate(self):
         """
         :return:
         """
+        div = 1
         expression = self.str_to_int_float(self.expression)
-        for num in expression:
-            while num not in operators.values():
-                self.result += num
+        count_add = 0
+        for i in range(len(expression)):
+            while expression[i] == '/':
+                expression[i - 1] /= expression[i + 1]
+                self.__uh(expression, i)
+                count_add += 2
                 break
-        return self.result
+
+        for i in range(len(expression)):
+            while expression[i] == '*':
+                expression[i - 1] *= expression[i + 1]
+                self.__uh(expression, i)
+                count_add += 2
+                break
+
+        for i in range(len(expression)):
+            while expression[i] == '-':
+                expression[i - 1] -= expression[i + 1]
+                self.__uh(expression, i)
+                count_add += 2
+                break
+
+        for i in range(len(expression)):
+            while expression[i] == '+':
+                expression[i - 1] += expression[i + 1]
+                self.__uh(expression, i)
+                count_add += 2
+                break
+
+        return f'{str(self)} = {expression[-1]}'
+        #
+        # return f'{expression} = {count_add}\ncalculate = {expression[-1]}'
