3bdc3f68953d81a1d72a18031fb01f1d7dd5f5dc
ankiav <katser.mike@gmail.com>
2022-11-16 01:51:28 +0300

    HW_18_Django

--- Django_HW/config/settings.py
+++ Django_HW/config/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
--- Django_HW/config/urls.py
+++ Django_HW/config/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# import sys
+# sys.path.append(r'/media/ankiav/Disc/PyProjects/TMS/HW/Katser_TeachMeSkills_Homeworks/Django_HW')
+
+import mainapp.views as mainapp
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.main),
 ]
